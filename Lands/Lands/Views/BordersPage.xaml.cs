b3851775e3e3aea8e13d57440cee03eb3304c9b0
miguelpz <miguelpz@gmail.com>
2018-09-26 14:03:55 +0200

    Implementación Tabs

---
+++ Lands/Lands/Views/BordersPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+namespace Lands.Views
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BordersPage : ContentPage
+	{
+		public BordersPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ Lands/Lands/Views/LandTabbedPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Xamarin.Forms.Xaml;
+
+namespace Lands.Views
+{
+    using Xamarin.Forms;
+
+    public partial class LandTabbedPage : TabbedPage
+	{
+		public LandTabbedPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
